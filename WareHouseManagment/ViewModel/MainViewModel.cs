b472e0fbc18833e27ff2429c04b75f211e55ef5b
toanitsv <khoailangdaknong@hotmail.com>
2019-02-19 15:02:18 +0700

    Project Created

---
+++ WareHouseManagment/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WareHouseManagment.ViewModel
+{
+    // Xử lý dữ liệu của màn hình MainWindow
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public MainViewModel()
+        {
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                LogInWindow loginWindow = new LogInWindow();
+                loginWindow.ShowDialog();
+            }
+           //MessageBox.Show("Access to MainViewModel --> DataContext's MainWindow.xaml", "WareHouseManagment", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
